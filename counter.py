395fe0f089827c6f6c5d6729d3ea6f658101714f
Susan Sandbothe <Susan.Sandbothe@vu.com>
2023-04-02 15:36:17 -0500

    counter

--- counter.py
+++ counter.py
@@ -3,7 +3,24 @@ def countEmail():
     name = input("Enter file:")
     if len(name) < 1 : name = "mbox-short.txt"
     handle = open(name)
-        
+
+    book = {}
+
+    for lin in handle: 
+        lin = lin.rstrip()
+        if lin.startswith("From "):
+            sender = lin.split()[1]
+
+            if sender not in book: 
+                book[sender] = 1
+            else: 
+                book[sender] += 1
+
+                    
+    largest = book.values()
+    maxval = max(largest)
+
+    print(max(book, key=book.get), maxval)
 
 ## if you want to test locally run > python counter.py
 if __name__ == "__main__":
